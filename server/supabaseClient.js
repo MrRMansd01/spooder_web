08dd81ee3ce5c5862bc4b97ab95a6abc950e0ec4
Mr.Rsd01 <mr.rsd01man@gmail.com>
2025-07-10 09:36:15 +0330

    Update supabaseClient.js

--- server/supabaseClient.js
+++ server/supabaseClient.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://lholzspyazziknxqopmi.supabase.co';
+const supabaseUrl = 'SUPABASE_URL';
 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxob2x6c3B5YXp6aWtueHFvcG1pIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjAyNzQxMCwiZXhwIjoyMDU3NjAzNDEwfQ.14HkUztZeFreB3fA3WVxdOOzcN8V1mROkZE6bJ0UOkk';
+const supabaseKey = 'SUPABASE_KEY';
 
 
 const supabase = createClient(supabaseUrl, supabaseKey, {
